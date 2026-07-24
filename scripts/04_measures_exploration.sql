0eb54e87ba3ef435b93abd6140cb803caeeae2fd
Y2Koi <139831910+Y2Koi@users.noreply.github.com>
2026-07-24 15:07:26 -0700

    Update 04_measures_exploration.sql

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -12,26 +12,25 @@ SQL Functions Used:
 */
 
 -- Find the Total Sales
-SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales
+SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales;
 
 -- Find how many items are sold
-SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales
+SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales;
 
 -- Find the average selling price
-SELECT AVG(price) AS avg_price FROM gold.fact_sales
+SELECT AVG(price) AS avg_price FROM gold.fact_sales;
 
 -- Find the total number of orders
-SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales
-SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales
+SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales;
 
 -- Find the total number of products
-SELECT COUNT(product_name) AS total_products FROM gold.dim_products
+SELECT COUNT(product_name) AS total_products FROM gold.dim_products;
 
 -- Find the total number of customers
-SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers
+SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
 
 -- Find the total number of customers that has placed an order
-SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
 
 -- Generate a report that shows all key metrics of the business
 SELECT 'Total Sales' as measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
@@ -44,4 +43,4 @@ SELECT 'Total Nr. Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
 UNION ALL
 SELECT 'Total Nr. Products', COUNT(product_name) FROM gold.dim_products
 UNION ALL
-SELECT 'Total Nr. Customers', COUNT(customer_key) FROM gold.dim_customers
+SELECT 'Total Nr. Customers', COUNT(customer_key) FROM gold.dim_customers;
